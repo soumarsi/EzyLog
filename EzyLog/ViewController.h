7d61adc5668d04ab7508a2d6b3a0b05f5f085c37
Soumen <admin@Soumens.local>
2015-06-15 16:25:07 +0530

    Initial Commit

---
+++ EzyLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EzyLog
+//
+//  Created by Soumen on 15/06/15.
+//  Copyright (c) 2015 Esolz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EzyLog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EzyLog
+//
+//  Created by Soumen on 15/06/15.
+//  Copyright (c) 2015 Esolz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
